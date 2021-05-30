7fab6446b9a9d090dcd6cafeabcc04b972f44b09
pps-am\gayane.hovhannisyan <gayane.hovhannisyan@praemium.com>
2021-05-31 00:24:56 +0400

    updating postman collection

--- food-ordering-api/restaurants.js
+++ food-ordering-api/restaurants.js
@@ -2,18 +2,33 @@ const restaurants = [{
 	name: "restaurant1",
 	description: "restaurant2",
 	workingHours: "10am to 10pm",
+	menuItems:[{
+		name:"Menue 1",
+		description:"Menue 1 desc",
+		price:1000
+	}],
 	logoImage: ""
 },
 {
 	name: "restaurant2",
 	description: "restaurant2",
 	workingHours: "10am to 10pm",
+	menuItems:[{
+		name:"Menue 2",
+		description:"Menue 2 desc",
+		price:1000
+	}],
 	"logoImage": ""
 },
 {
 	name: "restaurant3",
 	description: "restaurant3",
 	workingHours: "10am to 10pm",
+	menuItems:[{
+		name:"Menue 3",
+		description:"Menue 3 desc",
+		price:1000
+	}],
 	logoImage: ""
 }];
 
